8433e834349132387412bbe8c0caa6572ce84702
ricardojesus.vegas <ricardojesus.vegas@bbva.com>
2018-01-15 16:37:00 +0100

    Add examples

---
+++ examples/test_chart.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/rvegas/gchart"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	testChart := gchart.NewLineChart()
+	testChart.SetHeight(400)
+	testChart.SetWidth(900)
+	testChart.SetTitle("TESTING")
+
+	testChart.AddHeader("date", "date")
+	testChart.AddHeader("amount", "number")
+	testChart.SetCurvedLine()
+
+	testChart.LoadCSV("/home/ricardovegas/go/src/github.com/rvegas/gchart/weight.csv", false)
+
+	result, err := testChart.Generate()
+	check(err)
+
+	file, _ := os.Create("/tmp/index.html")
+	fmt.Fprint(file, result)
+
+	fmt.Println("Done")
+	//exec.Command("xdg-open", fmt.Sprintf("file:///%s", file.Name())).Start()
+}
+
+// fatal if there is an error
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
